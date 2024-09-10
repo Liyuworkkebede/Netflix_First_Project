a71272e786efcb1cbabf6d42f78f3c894c93441d
liyuwork <liyuworkkebede21@gmail.com>
2024-09-11 00:27:42 +0300

    banner

--- Netflix/src/pages/Home/Home.jsx
+++ Netflix/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from "../../Components/Header/Header.jsx"
 import Footer from "../../Components/Footer/Footer.jsx"
+import Banner from "../../Components/Banner/banner.jsx"
 function Home() {
   return (
     <>
     <Header/>
+    <Banner/>
     <Footer/>
     </>
   )
 }
 
-export default Home
+export default Home;
